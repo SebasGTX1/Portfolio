1a4cec09808326c310ecbe06787e852b25700a3e
SebasGTX1 <sebas-0202@hotmail.es>
2023-06-28 16:43:30 -0500

    Basic funtional structure for my personal portfolio

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Title')
+    description = models.TextField(max_length=1000, verbose_name='Description')
+    image = models.ImageField(verbose_name='Project image', upload_to='projects')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Update date')
+    url = models.URLField(null=True, blank=True, verbose_name='Project URL')
+
+    class Meta:
+        verbose_name = "project"
+        verbose_name_plural = "projects"
+        ordering = ['-created']
+    
+    def __str__(self) -> str:
+        return self.title
