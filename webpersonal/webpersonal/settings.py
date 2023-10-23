ef1d108e896cd7b365707878678cb3ea36c65c9b
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-23 00:25:10 -0500

    Improving path configuration with os

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 #import os
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.as_posix() +'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR.as_posix(), 'db.sqlite3')
     }
 }
 
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.as_posix() +'/static/'
+STATIC_ROOT = os.path.join(BASE_DIR.as_posix(), 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -129,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.as_posix() +'/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR.as_posix(), 'media')
